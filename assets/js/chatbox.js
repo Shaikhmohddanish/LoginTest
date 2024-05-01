529bc71a0c50a51a21a63dc2f26aed59d727bf80
ShaikhMohdDanish <danishshaikh2365@gmail.com>
2024-05-01 14:49:01 +0530

    Added

---
+++ assets/js/chatbox.js
@@ -0,0 +1,5 @@
+function loadChatgen(){var e=window.ChatGen||[];let t=new URLSearchParams(window.location.search),a={};for(let[n,i]of t)-1==["server","key"].indexOf(n)&&(a[n]=i);if(window.chatgenSettings||(window.chatgenSettings={}),window.chatgenSettings={...a,...window.chatgenSettings},!e.loaded){var c=document.createElement("script");c.type="text/javascript",c.async=!0,c.src="https://app.chatgen.ai/cmp/chat-widget/bot.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(c,r),c.onload=function(){/iPhone|iPad|iPod|Android/i.test(navigator.userAgent),ChatGen.init({widget_key:"9l0PLPKc"})}}}setTimeout(loadChatgen,CUSTOM_DELAY);
+
+setTimeout(() => {
+	$("#selekt-chat-widget").addClass('mob-fix');
+}, 5000);
